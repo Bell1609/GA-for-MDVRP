c847aed5927424cac8b9f54068d4ce4803208d3b
Bell <85625700+Bell1609@users.noreply.github.com>
2024-05-08 16:24:33 +0700

    Add files via upload

---
+++ utility.py
@@ -0,0 +1,4 @@
+# Description: This file contains utility functions that are used in the main program.
+import math
+def distance(pos1, pos2):
+    return math.sqrt((pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2)
